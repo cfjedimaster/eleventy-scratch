c467ab97d03cf506ef78d6245aedf6981dedcb40
Raymond Camden <raymondcamden@gmail.com>
2025-04-30 15:46:57 -0500

    testing

--- src/_data/apidata.js
+++ src/_data/apidata.js
@@ -1,5 +1,14 @@
+import Fetch from "@11ty/eleventy-fetch";
+
 export default async () => {
 
+	/*
 	let req = await fetch('https://raymondcamden-placidyellowptarmigan.web.val.run/');
 	return await req.json();
+	*/
+	
+	return await Fetch('https://raymondcamden-placidyellowptarmigan.web.val.run/', {
+		duration:'1d',
+		type:'json'
+	});
 }
